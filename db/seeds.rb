fba4662ce451fcfbbc4d9881e43a80d41d4bb954
iman fuad <nooreliman@gmail.com>
2021-11-24 14:35:17 +0800

    added reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "starting to seed"
+Restaurant.destroy_all
+20.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+end
+
+puts "Done seeding. Total restaurants created: #{Restaurant.count}"
